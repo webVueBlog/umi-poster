54275bde9b8ed3bcdb7163e02839adf28ecd4ecd
webVueBlog <2979986590@qq.com>
2022-08-16 12:25:29 +0800

    fix: 修改优化

--- .umirc.ts
+++ .umirc.ts
@@ -1,5 +1,7 @@
 export default {
   npmClient: 'yarn',
   title: '海报生成器',
+  base: process.env.NODE_ENV === "production" ? "/umi-poster/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/umi-poster/" : "/",
   extraBabelPlugins: [["import", { libraryName: "antd", style: true }]],
 };
